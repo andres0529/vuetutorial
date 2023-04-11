0adbd175939a455a4a5fdbe81d35a45cb66203a2
Andres Correa <andrescorrea.m@hotmail.com>
2023-04-11 15:29:55 -0400

    Exercise done

--- script.js
+++ script.js
@@ -2,15 +2,24 @@ const API = "https://api.github.com/users/";
 const app = Vue.createApp({
   data() {
     return {
-      name: "Andres",
-      url:"myportfolio.com.co",
       search: null,
       result: null,
       error: null,
       favorites: new Map(),
     };
   },
-  computed: {},
+  computed: {
+    isFavorite() {
+      return this.favorites.has(this.result.id);
+    },
+    allFavorites() {
+      // for (const favorite of favoritesArray) {
+      //   console.log(favorite.name);
+      //   console.log(favorite.url); 
+      // }
+      return Array.from(this.favorites.values());
+    },
+  },
   methods: {
     async doSearch() {
       this.result = this.error = null;
@@ -25,6 +34,13 @@ const app = Vue.createApp({
         this.search = null;
       }
     },
+    addFavorite() {
+      this.favorites.set(this.result.id, this.result);
+    },
+    removeFavorite() {
+      this.favorites.delete(this.result.id);
+    },
+
 
   },
 });
